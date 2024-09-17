3186b4413d66d997fc9c3a2d1edd0bec7acdc85b
mohamed hasan <87263653+mo7medhasan@users.noreply.github.com>
2024-09-17 21:56:57 +0300

    add change

--- src/components/Home/DailySaleComponent.jsx
+++ src/components/Home/DailySaleComponent.jsx
@@ -66,13 +66,13 @@ export default function DailySaleComponent({ categories }) {
           marginBottom: "20px",
         }}
         >
-          {/* <div>
+          <div>
             <div className={styles.title} style={{ marginBottom: "5px" }}>
               Daily Sale
             </div>
             <CountdownTimer hours={5} minutes={30} seconds={20} type="b" />
           </div>
-          <Star type="b" /> */}
+          <Star type="b" />
         <div
           onClick={() => navigate("/search?sale=true")}
           style={{
