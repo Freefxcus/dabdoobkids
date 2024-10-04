c21ba22793d48b3e03fb6fda5404c7f1a6a1a04d
mohamed hasan <87263653+mo7medhasan@users.noreply.github.com>
2024-10-04 20:50:55 +0300

    add wallet

--- src/components/profile/WalletComponents.jsx
+++ src/components/profile/WalletComponents.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Loader from "../Loader";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { getWallet, getWalletHistory } from "../../utils/apiCalls";
 import styles from "../../styles/pages/Profile.module.css";
 
@@ -9,6 +9,8 @@ const WalletComponents = () => {
   const [walletHistory, setWalletHistory] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const options = { day: '2-digit', month: 'short', year: 'numeric' };
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -30,27 +32,20 @@ const WalletComponents = () => {
   }, []);
 
   return (
-    <div
-      style={{
-        width: "100%",
-      }}
-    >
+    <div style={{ width: "100%" }}>
       {isLoading && <Loader open={true} />}
       {isError && <div>No Orders Created ...</div>}
       {wallet && (
-        <Box
+        <Stack
           className={styles.text_container}
           style={{
             boxShadow: "rgba(0, 0, 0, 0.05) 0px 1px 2px 0px",
             backgroundColor: "#fff",
             padding: "20px",
             borderRadius: "8px",
-            // display: "flex",
-            // flexDirection: "column",
-            // gap: "15px",
           }}
         >
-          <Box
+          <Box component={"div"}
             sx={{
               display: "flex",
               justifyContent: "space-between",
@@ -65,7 +60,7 @@ const WalletComponents = () => {
                   fontWeight: "600",
                 }}
               >
-                wallet
+                Wallet
               </Box>
               <Box
                 sx={{
@@ -79,43 +74,41 @@ const WalletComponents = () => {
             </Box>
           </Box>
 
-          {walletHistory?.items?.length > 0 ? (
-            <Box sx={{
-                width:"100%",
-                overflowX:"auto"
-            }}>
-              {" "}
-              <table className={styles.my_table}>
-                <tr>
-                  <th>Created</th>
-                  <th>Type</th>
-                  <th>Details</th>
-                  <th>Amount</th>
-                  <th>Balance</th>
-                </tr>
-                {walletHistory?.items?.map((item, index) => (
-                  <tr>
-                    <td> 10 Mar 2024
-                     {/* {  new Date(item?.createdAt).toLocaleString() } */}
-                    </td>
-                    <td>Order Fee</td>
-                    <td>Process credits against NEGG20093079663.</td>
-                    <td
-                      style={{
-                        color: index === 2 ? "#F04438" : "#32D583",
-                      }}
-                    >
-                      - EGP 7000
-                    </td>
-                    <td>EGP {item?.wallet}</td>
-                  </tr>
-                ))}
-              </table>{" "}
-            </Box>
-          ) : (
-            <h1>No Wallet History</h1>
-          )}
-        </Box>
+          <Stack  sx={{ position: "relative", flex:1,maxWidth:{xs:"80vw",sm:"60vw"}}}>
+            {walletHistory?.items?.length > 0 ? (
+              <Stack sx={{ overflowX: "auto", maxWidth: "100%" }}>
+                <table className={styles.my_table}>
+                  <thead>
+                    <tr>
+                      <th>Created</th>
+                      <th>Details</th>
+                      <th>Amount</th>
+                      <th>Balance</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {walletHistory?.items?.map((item, index) => (
+                      <tr key={index}>
+                        <td>{new Date(item?.createdAt)?.toLocaleDateString('en-GB', options)}</td>
+                        <td>Process credits against {item?.order}.</td>
+                        <td
+                          style={{
+                            color: item?.credits?.startsWith("-")  ? "#F04438" : "#32D583",
+                          }}
+                        >
+                          {item?.credits} EGP
+                        </td>
+                        <td>{item?.wallet} EGP</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </Stack>
+            ) : (
+              <h1>No Wallet History</h1>
+            )}
+          </Stack>
+        </Stack>
       )}
     </div>
   );
