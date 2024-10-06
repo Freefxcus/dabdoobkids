f21ae6085995991339394f4c48f8913e4371a813
ziad tarek <66745488+ziadtarek112@users.noreply.github.com>
2024-10-06 19:15:41 +0200

    bottom section style fix

--- src/components/OfferCard.jsx
+++ src/components/OfferCard.jsx
@@ -4,9 +4,9 @@ import styles from "../styles/components/OfferCard.module.css";
 export default function OfferCard({ item }) {
   return (
     <div className={styles.container}>
-      <div className={styles["image-container"]}>
         {/* <img src={item.img} width="150px" /> */}
-        <img loading="lazy" src={item.img}  alt="offer"/>
+      <div className={styles["image-container"]}>
+        <img style={{width:"100%"}} loading="lazy" src={item.img}  alt="offer"/>
       </div>
       <div className={styles.title}>{item.title}</div>
       <div className={styles.description}>{item.body}</div>
