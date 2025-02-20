ac4c277778c07f2b99543b9e87a1107732a2da46
Abdelrahman <abdelrahmanfarag1980@gmail.com>
2025-02-21 00:28:15 +0100

    Fixing merge conflicts

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -280,8 +280,8 @@ export default function Form({ type, toggleDrawer }) {
         <>
           <div className={styles.title}>Create an Account</div>
           <div className={`${styles.sub_title} ${styles.bottom_margin}`}>
-            Register for faster checkout, track your order's status, and more .
-          </div>
+            Register for faster checkout, track your order's status, and more
+          s</div>
           {/* first name */}
           <div className={styles.label}>
             <span>First name</span>
