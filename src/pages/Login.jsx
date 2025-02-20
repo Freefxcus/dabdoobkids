5fb1c279bc1b442f359369df7881923a47e9dad4
Abdelrahman <abdelrahmanfarag1980@gmail.com>
2025-02-20 23:33:00 +0100

    sync with remote

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styles from "../styles/pages/Register.module.css";
 import Form from "../components/Form";
 import { Helmet } from "react-helmet";
@@ -22,24 +21,3 @@ export default function Login() {
     </div>
   );
 }
-=======
-import { useState } from "react";
-import styles from "../styles/pages/Register.module.css";
-import Form from "../components/Form";
-import { Helmet } from "react-helmet";
-
-export default function Login() {
-  const [largeImage, setLargeImage] = useState();
-
-  return (
-    <div
-      className={`${styles.container} section-top-padding section-bottom-padding`}
-    >  <Helmet>
-        <title>{"Dabdoob Kidz | login page"}</title>
-        <meta name="description" content={"Join the Dabdoob Kidz Fun! login for your child's free account and unlock a world of exciting activities, games, and learning adventures designed especially for kids"} />
-      </Helmet>
-      <Form type="login" />
-    </div>
-  );
-}
->>>>>>> d1589dedd (Saving local changes before pulling latest updates)
