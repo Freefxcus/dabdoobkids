5b2b2bc98416cc176b10e0639b5cf8ef62165270
Abdelrahman <abdelrahmanfarag1980@gmail.com>
2025-03-13 04:07:55 +0100

    SEO - Fixing index.html

--- src/Redux/wishlistApi.jsx
+++ src/Redux/wishlistApi.jsx
@@ -27,7 +27,7 @@ export const wishlistApi = createApi({
     getAllWishList: builder.query({
       query: () => {
         const token = localStorage.getItem("access_token");
-        if (!token) return null;           
+        if (!token) return { data: [] };          
         return `/wishlists`;
       },
       providesTags: ["Wishlist"],
