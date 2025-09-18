8d8dcf4efb61735b74e2993b26a7d752819cb05c
Abdelrahman <abdelrahmanfarag1980@gmail.com>
2025-09-18 08:59:53 +0300

    Rolling back paymob - temp

--- src/components/checkout/ConfirmPayment.jsx
+++ src/components/checkout/ConfirmPayment.jsx
@@ -128,7 +128,7 @@ export default function ConfirmPayment({
         notifySuccess("Order Placed Successfully");
 
         deleteAllCart().then(() => {
-          navigate(ThankYou);
+          navigate("/");
         });
       }
       setLoading(false);
